3a468d527c144f8f2c2d6b399151d93b9ce9d4be
ivan chavez <iecs_1990@hotmail.com>
2017-01-26 22:49:47 -0700

    crawler v0.2.0

---
+++ crawlerpages/SeccionAmarilla.php
@@ -0,0 +1,73 @@
+<?php
+namespace CrawlerPages;
+use Sunra\PhpSimple\HtmlDomParser;
+
+class SeccionAmarilla {
+    public $visitLink       = ["#"]; //urls analizadas
+    public $links           = []; //urls por analizar
+    public $crawlerCounter  = 0;
+    public $business        = []; //informacion extraida
+    public $lastCounterBus  = 0;
+    public $domain          = "https://www.seccionamarilla.com.mx";
+    
+    public $linkCrawler     = [
+        "https://www.seccionamarilla.com.mx/resultados/restaurantes/1"
+    ];
+    
+
+    public function crawler($newLink = "") {
+        if($newLink == "")
+            $str = $this->getSslPage($this->linkCrawler[0]);
+        else 
+            $str = $this->getSslPage($newLink);
+
+        $dom        = HtmlDomParser::str_get_html($str);
+        $this->getData($dom); //obtiene la informacion de la primer pagina
+    }
+
+    public function getData($dom) {
+        $this->analizeBusiness($dom);
+        $this->analizePagination($dom);
+        $this->crawlerCounter++;
+    }
+    
+    public function analizeBusiness($dom) {
+        foreach($dom->find("ul.list li") as $element) {
+            $phone = $element->find(".l-tel a span");
+            $data = [
+                "name"      => $element->find(".l-datos a h2 span")[0]->innertext,
+                "website"   => $element->find(".l-btn-container a")[0]->href,
+                "address"   => $element->find(".l-datos .l-address span")[0]->innertext,
+                "neibor"    => $element->find(".l-datos .l-address span")[1]->innertext,
+                "city"      => $element->find(".l-datos .l-address span")[2]->innertext,
+                "state"     => $element->find(".l-datos .l-address span")[3]->innertext,
+                "phone"     => isset($phone[0]->href) ? $phone[0]->href : "",
+                "infopage"  => $element->find(".l-datos a")[0]->href
+            ];
+
+            $this->business[] = $data;
+        }
+    }
+
+    public function analizePagination($dom) {
+        foreach($dom->find(".pagination li a") as $link) {
+            if(!in_array($link->href, $this->visitLink)) {
+                $this->links[] = $link->href;
+            }
+        }
+    }
+
+    public function getSslPage($url) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_REFERER, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $str = curl_exec($curl);
+        curl_close($curl);
+        return $str;
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,72 +1,39 @@
 <?php 
     include "vendor/autoload.php";
+    include "crawlerpages/SeccionAmarilla.php";
+    use CrawlerPages\SeccionAmarilla;
 
-    use Sunra\PhpSimple\HtmlDomParser;
-    
     set_time_limit(3000);
-
+    @session_start();
     function dd($arr) {
         echo "<pre>";
         print_r($arr);
         echo "</pre>"; exit;
     }
 
-    function get_business($dom) {
-        $business = [];
-         foreach($dom->find("ul.list li") as $element) {
-            $phone = $element->find(".l-tel a span");
-            $data = [
-                "name"      => $element->find(".l-datos a h2 span")[0]->innertext,
-                "website"   => $element->find(".l-btn-container a")[0]->href,
-                "address"   => $element->find(".l-datos .l-address span")[0]->innertext,
-                "neibor"    => $element->find(".l-datos .l-address span")[1]->innertext,
-                "city"      => $element->find(".l-datos .l-address span")[2]->innertext,
-                "state"     => $element->find(".l-datos .l-address span")[3]->innertext,
-                "phone"     => isset($phone[0]->href) ? $phone[0]->href : "",
-                "infopage"  => $element->find(".l-datos a")[0]->href
-            ];
+    $crawler = new SeccionAmarilla();
 
-            $business[] = $data;
+    $crawler->crawler(); //init page
+
+    foreach($crawler->links as $link) {
+        if(!in_array($link, $crawler->visitLink)) {
+            $crawler->crawler($crawler->domain.$link);
+
+           if(count($crawler->business) >= 100) 
+               break;
+           
         }
-
-        return $business;
     }
 
-    function getSslPage($url) {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_REFERER, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $str = curl_exec($curl);
-        curl_close($curl);
-        return $str;
-    }
-
-    $str = getSslPage("https://www.seccionamarilla.com.mx/resultados/restaurantes/1");
     
-    if($str !== "") {
-        $dom = HtmlDomParser::str_get_html($str);
-        $businessData   = [];
-        $visitLink      = ["#"];
-        $businessData   = get_business($dom);
-
-        foreach($dom->find(".pagination li a") as $link) {
-            if(!in_array($link->href, $visitLink)) {
-                $visitLink[] = $link->href;
-
-                $strNext    = getSslPage("https://www.seccionamarilla.com.mx".$link->href);
-                $domNext    = HtmlDomParser::str_get_html($str);
-                $items      = get_business($domNext);
-            }
-        }
-        
-        exit;
-
+    if(isset($_SESSION["permanentData"]) && $_SESSION["permanentData"] !== ""){
+        $oldData = json_decode($_SESSION["permanentData"]);
+        $newData = array_merge($oldData, $crawler->business);
+        $_SESSION["permanentData"] = json_encode($newData);
     } else {
-        echo "pagina vacia";
+        $_SESSION["permanentData"] = json_encode($crawler->business);
     }
+
+    dd($crawler->business);
 
 ?>
